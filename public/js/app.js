79521fae2f805ac4abaab5085279a45131dbc27f
Ваше Имя <akimowiljagg@outlook.com>
2025-05-08 11:30:21 +0300

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,108 @@
+document.addEventListener("DOMContentLoaded", function () {
+    //! Создание заметки (прототип)
+    const list = document.querySelector(".create-new__list");
+    const newPage = document.querySelector(".new__page");
+    list.addEventListener("click", function () {
+        if (list) {
+            list.style.display = "none";
+            newPage.style.display = "block";
+        }
+        // else {
+        //     list.style.display = "block";
+        //     newPage.style.display = "none";
+        // }
+    });
+
+    //! Обработчик поиска
+    const searchInput = document.querySelector(".search__input");
+    searchInput.addEventListener("input", function (e) {
+        const searchTerm = e.target.value.toLowerCase();
+        console.log("Поиск:", searchTerm);
+    });
+
+    //! Обработчики для правой части навигации
+    const navRightIcons = document.querySelectorAll(".nb__right img");
+    navRightIcons.forEach((icon) => {
+        icon.addEventListener("click", function () {
+            const altText = this.alt;
+            switch (altText) {
+                case "Перезагрузка":
+                    location.reload();
+                    break;
+                case "Список":
+                    console.log("Показать список заметок");
+                    break;
+                case "Настройки":
+                    console.log("Открыть настройки");
+                    break;
+            }
+        });
+    });
+
+    //! Обработчик для меню (прототип)
+    document
+        .querySelector('.nb__left img[alt="Меню"]')
+        .addEventListener("click", function () {
+            console.log("Открытие бокового меню");
+        });
+
+    searchInput.addEventListener("focus", function () {
+        document.querySelector(".search").style.boxShadow =
+            "0 0 15px rgba(0,0,0,0.2)";
+    });
+
+    searchInput.addEventListener("blur", function () {
+        document.querySelector(".search").style.boxShadow =
+            "0px 0px 30px 10px rgba(172, 172, 172, 0.5)";
+    });
+});
+
+// ! Эффект печатающегося текста
+class TypeWriter {
+    constructor(elementId, texts, speed = 100, pause = 2000) {
+        this.element = document.getElementById(elementId);
+        this.texts = texts;
+        this.speed = speed;
+        this.pause = pause;
+        this.index = 0;
+        this.text = "";
+        this.isDeleting = false;
+        this.type();
+    }
+
+    type() {
+        const currentText = this.texts[this.index];
+
+        if (this.isDeleting) {
+            this.text = currentText.substring(0, this.text.length - 1);
+        } else {
+            this.text = currentText.substring(0, this.text.length + 1);
+        }
+
+        this.element.innerHTML = this.text + '<span class="cursor">|</span>';
+
+        let typeSpeed = this.speed;
+
+        if (this.isDeleting) {
+            typeSpeed /= 2;
+        }
+
+        if (!this.isDeleting && this.text === currentText) {
+            typeSpeed = this.pause;
+            this.isDeleting = true;
+        } else if (this.isDeleting && this.text === "") {
+            this.isDeleting = false;
+            this.index = (this.index + 1) % this.texts.length;
+            typeSpeed = 500;
+        }
+
+        setTimeout(() => this.type(), typeSpeed);
+    }
+}
+
+new TypeWriter(
+    "typing-effect",
+    ["Эта страница в разработке!", "Приносим свои извинения"],
+    100,
+    1500
+);
