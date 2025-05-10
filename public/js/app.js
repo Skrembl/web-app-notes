984ec7d0fb9cc6f243eba4505f53aaf10b53307a
Ваше Имя <akimowiljagg@outlook.com>
2025-05-10 14:05:50 +0300

    Initial commit

--- public/js/app.js
+++ public/js/app.js
@@ -52,8 +52,45 @@ document.addEventListener("DOMContentLoaded", function () {
     //! Плавное появление элементов при клике на меню
     const menuLeft = document.getElementById("menu-left");
     const leftItems = document.querySelector(".menu__left-items");
+    const createNewList = document.querySelector(".create-new__list");
 
     menuLeft.addEventListener("click", () => {
         leftItems.classList.toggle("slide");
+
+        //! Динамическая смена позиционирования блока
+        if (leftItems.classList.contains("slide")) {
+            createNewList.style.left = "calc(50% + 250px)";
+        } else {
+            createNewList.style.left = "50%";
+        }
     });
+
+    //! Добавление класса active к меню
+    const currentPath = window.location.pathname;
+    const menuLinks = document.querySelectorAll(".menu__left-items a");
+
+    menuLinks.forEach((link) => {
+        const linkPath = link.getAttribute("href");
+
+        if (currentPath === linkPath || currentPath.startsWith(linkPath)) {
+            link.parentElement.classList.add("active");
+        } else {
+            link.parentElement.classList.remove("active");
+        }
+    });
+
+    //! Если SPA приложение
+    // menuLinks.forEach((link) => {
+    //     link.addEventListener("click", function (e) {
+    //         e.preventDefault();
+
+    //         menuLinks.forEach((link) => {
+    //             link.parentElement.classList.remove("active");
+    //         });
+
+    //         this.parentElement.classList.add("active");
+
+    //         loadContent(this.href);
+    //     });
+    // });
 });
