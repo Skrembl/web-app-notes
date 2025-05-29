bd8ee7fe29cd17c9b1606a952c4f638db132722d
Ваше Имя <akimowiljagg@outlook.com>
2025-05-29 09:00:38 +0300

    Initial commit

--- public/js/app.js
+++ public/js/app.js
@@ -150,13 +150,157 @@ function initDynamicContent() {
     const newNoteBtn = document.querySelector(".create-new__list");
     const newNotePage = document.querySelector(".new__page");
 
-    if (newNoteBtn && newNotePage) {
+    if (newNoteBtn) {
         newNoteBtn.addEventListener("click", () => {
-            newNoteBtn.style.display = "none";
+            // newNoteBtn.style.display = "none";
             newNotePage.style.display = "block";
         });
     }
 }
+
+//! Создание заметки
+function popup() {
+    const popupContainer = document.createElement("div");
+
+    popupContainer.innerHTML = `
+    <div id="popupContainer">
+        <h1>Новая заметка</h1>
+        <textarea id="note-text" placeholder="Введите свой текст..."></textarea>
+        <div id="btn-container">
+            <button id="submitBtn" onclick="createNote()">Создать</button>
+            <button id="closeBtn" onclick="closePopup()">Закрыть</button>
+        </div>
+    </div>
+    `;
+    document.body.appendChild(popupContainer);
+}
+
+//! Закрытие заметки
+function closePopup() {
+    const popupContainer = document.getElementById("popupContainer");
+    if (popupContainer) {
+        popupContainer.remove();
+    }
+}
+
+//! Созданная заметка
+function createNote() {
+    const popupContainer = document.getElementById("popupContainer");
+    const noteText = document.getElementById("note-text").value;
+    if (noteText.trim() !== "") {
+        const note = {
+            id: new Date().getTime(),
+            text: noteText,
+        };
+
+        const existingNotes = JSON.parse(localStorage.getItem("notes")) || [];
+        existingNotes.push(note);
+
+        localStorage.setItem("notes", JSON.stringify(existingNotes));
+
+        document.getElementById("note-text").value = "";
+
+        popupContainer.remove();
+        displayNotes();
+
+        if (window.location.hash === "#/notes") {
+            displayNotes();
+        }
+    }
+}
+
+//! Показ заметок
+function displayNotes() {
+    const notesList = document.getElementById("notes-list");
+    if (!notesList) return;
+
+    notesList.innerHTML = "";
+
+    const notes = JSON.parse(localStorage.getItem("notes")) || [];
+
+    notes.forEach((note) => {
+        const listItem = document.createElement("li");
+        listItem.innerHTML = `
+        <span>${note.text}</span>
+        <div id="noteBtns-container">
+            <button id="editBtn" onclick="editNote(${note.id})"><i class="fa-solid fa-pen"></i></button>
+            <button id="deleteBtn" onclick="deleteNote(${note.id})"><i class="fa-solid fa-trash"></i></button>
+        </div>
+        `;
+        notesList.appendChild(listItem);
+    });
+}
+
+//! Редактирование заметок
+function editNote(noteId) {
+    const notes = JSON.parse(localStorage.getItem("notes")) || [];
+    const noteToEdit = notes.find((note) => note.id == noteId);
+    const noteText = noteToEdit ? noteToEdit.text : "";
+    const editingPopup = document.createElement("div");
+
+    editingPopup.innerHTML = `
+    <div id="editing-container" data-note-id="${noteId}">
+        <h1>Edit Note</h1>
+        <textarea id="note-text">${noteText}</textarea>
+        <div id="btn-container">
+            <button id="submitBtn" onclick="updateNote()">Готово</button>
+            <button id="closeBtn" onclick="closeEditPopup()">Отмена</button>
+        </div>
+    </div>
+    `;
+
+    document.body.appendChild(editingPopup);
+}
+
+function closeEditPopup() {
+    const editingPopup = document.getElementById("editing-container");
+
+    if (editingPopup) {
+        editingPopup.remove();
+    }
+}
+
+function updateNote() {
+    const noteText = document.getElementById("note-text").value.trim();
+    const editingPopup = document.getElementById("editing-container");
+
+    if (noteText !== "") {
+        const noteId = editingPopup.getAttribute("data-note-id");
+        let notes = JSON.parse(localStorage.getItem("notes")) || [];
+
+        const updatedNotes = notes.map((note) => {
+            if (note.id == noteId) {
+                return { id: note.id, text: noteText };
+            }
+            return note;
+        });
+
+        localStorage.setItem("notes", JSON.stringify(updatedNotes));
+
+        editingPopup.remove();
+
+        displayNotes();
+
+        if (window.location.hash === "#/notes") {
+            displayNotes();
+        }
+    }
+}
+
+//! Удаление заметок
+function deleteNote(noteId) {
+    let notes = JSON.parse(localStorage.getItem("notes")) || [];
+    notes = notes.filter((note) => note.id !== noteId);
+
+    localStorage.setItem("notes", JSON.stringify(notes));
+    displayNotes();
+
+    if (window.location.hash === "#/notes") {
+        displayNotes();
+    }
+}
+
+displayNotes();
 
 class Router {
     constructor(routes) {
@@ -198,6 +342,10 @@ class Router {
             setTimeout(() => initDateUpdater(), 10);
         } catch (e) {
             this.showError();
+        }
+
+        if (path === "/notes") {
+            displayNotes();
         }
     }
 
